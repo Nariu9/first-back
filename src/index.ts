ec21f21004634263580b2a12a1a3596a4dd21fdc
Artem Karpukhin <a.karpukhin9@gmail.com>
2023-01-18 13:38:19 +0200

    feat: add middlewares according to chain of responsibility patter

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, {NextFunction, Response, Request} from 'express'
 import bodyParser from 'body-parser';
 import {productsRouter} from './routes/products-router';
 import {addressesRouter} from './routes/addresses-router';
@@ -12,6 +12,28 @@ app.use(parserMiddleware)
 
 app.use('/products', productsRouter)
 app.use('/addresses', addressesRouter)
+
+const authGuardMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    if (req.query.token === '123') {
+        next()
+    } else {
+        res.send(401)
+    }
+}
+
+let requestCounter = 0
+
+const requestCounterMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    requestCounter++
+    next()
+}
+
+app.use(requestCounterMiddleware)
+app.use(authGuardMiddleware)
+
+app.get('/', (req, res) => {
+    res.send({value: 'count of requests ' + requestCounter})
+})
 
 
 app.listen(port, () => {
